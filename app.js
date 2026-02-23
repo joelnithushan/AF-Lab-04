2341d2044bfe929a3d935ea25f6ce2c1d6a5c2fd
joelnithushan <joelnithushan6@gmail.com>
2026-02-23 08:55:14 +0530

    app.js updated for read file.txt

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Node js app");
+//console.log("Node js app");
+
+const fs = require("fs");
+
+fs.readFile("file.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
